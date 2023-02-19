c88f8bd373ae831d20245e5ff74da96917275546
utkubayguven <79966502+utkubayguven@users.noreply.github.com>
2023-02-19 15:49:02 +0300

    edit

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const authRoute = require("./routes/auth")
+
+dotenv.config();
+mongoose.set('strictQuery', false);
+mongoose
+    .connect(process.env.MONGO_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    })
+    .then(()=>console.log("DB Connection Successful!"))
+    .catch((err)=> console.log(err));
+
+app.use(express.json())
+app.use("/api/auth", authRoute);
 
 app.listen(8000,()=>{
-    console.log("backend server working...")
-})
+    console.log("Backend server working...")
+});
