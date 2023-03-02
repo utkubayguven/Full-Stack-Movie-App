a692eae89015e52053e59edde71f385d3c79242e
utkubayguven <79966502+utkubayguven@users.noreply.github.com>
2023-03-02 23:56:20 +0300

    update

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,22 +3,31 @@ const User = require("../models/User")
 const CryptoJS = require("crypto-js")
 const validate = require("../validateToken")
 
-router.put("/:id",validate,async(req,res)=>{
-    if(req.user.id === req.params.id || req.user.isAdmin){
-        if(req.body.password){
-            req.body.password = CryptoJS.AES.encrypt(req.body.password,process.env.SECRET_KEY)
-        }
-
-        try{
-            const updatedUser = await User.findByIdAndUpdate(req.params.id,{$set: req.body})
-            res.status(200).json(updatedUser)
-        }catch(err){
-            res.status.json(err)
-        }
-    }else{
-        res.status.json(403).json("You are not admin")
+router.put("/:id", validate, async (req, res) => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      if (req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(
+          req.body.password,
+          process.env.SECRET_KEY
+        ).toString();
+      }
+  
+      try {
+        const updatedUser = await User.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        );
+        res.status(200).json(updatedUser);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(403).json("You can update only your account!");
     }
-})
+  });
 
 
 module.exports = router
