2ea6b20da3af81b0b1080173e105e9af57f26e05
utkubayguven <79966502+utkubayguven@users.noreply.github.com>
2023-03-05 16:11:29 +0300

    trim

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,25 +1,17 @@
 const router = require("express").Router();
-const User = require("../models/User")
-const CryptoJS = require("crypto-js")
-const validate = require("../validateToken")
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+const validate = require("../validateToken");
 
 router.put("/:id", validate, async (req, res) => {
-  if (req.user.id === req.params.id || req.user.isAdmin) {
+
+  if (req.user.id.trim() === req.params.id.trim() || req.user.isAdmin) {
     if (req.body.password) {
-      req.body.password = CryptoJS.AES.encrypt(
-        req.body.password,
-        process.env.SECRET_KEY
-      ).toString();
+      req.body.password = CryptoJS.AES.encrypt(req.body.password,process.env.SECRET_KEY).toString();
     }
 
     try {
-      const updatedUser = await User.findByIdAndUpdate(
-        req.params.id,
-        {
-          $set: req.body,
-        },
-        { new: true }
-      );
+      const updatedUser = await User.findByIdAndUpdate(req.params.id.trim(),{$set: req.body,},{ new: true });
       res.status(200).json(updatedUser);
     } catch (err) {
       res.status(500).json(err);
@@ -29,5 +21,6 @@ router.put("/:id", validate, async (req, res) => {
   }
 });
 
+module.exports = router;
 
-module.exports = router
+
--- api/validateToken.js
+++ api/validateToken.js
@@ -1,18 +1,18 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-function validate(req,res,next){
-    const authHeader = req.headers.token;
-    if(authHeader){
-        const token = authHeader.split(" ")[1]
+function validate(req, res, next) {
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
 
-        jwt.verify(token,process.env.SECRET_KEY,(err,user)=>{
-            if(err) res.status(403).json("token is not valid!");
-            req.user = user;
-            next()
-        })
-    }else{
-        return res.status(401).json("something went wrong !!!")
-    }
+    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+      if (err) res.status(403).json("Token is not valid!");
+      req.user = user;
+      next();
+    });
+  } else {
+    return res.status(401).json("You are not authenticated!");
+  }
 }
 
 module.exports = validate;
